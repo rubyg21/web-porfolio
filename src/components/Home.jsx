1c3d61e49c9f66b610eb8514a895b341c2470bc0
Rubens Galarraga <rgalarraga121@gmail.com>
2022-11-18 16:41:48 -0300

    udate

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,34 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 import { NavLink } from "react-router-dom";
-import foto from '../assets/foto.png'
+import foto from "../assets/foto.png";
 
 const Home = () => {
   return (
-<div className='Home'>
+    <div className="Home">
+      <div className="Iam">
+        <h1>
+          HI I'm <br /> Rubens Galarraga <br /> Web Developer
+        </h1>
+        <p className="subtitulo">
+          Frontend Developer / Javascript / Vue js / React/ Node js
+        </p>
 
+        <div>
+          <NavLink to="/contact" className="contactme">
+            Contact me
+          </NavLink> <span></span>
+          <NavLink to="/contact" className="cv">
+            Download CV
+          </NavLink>
+        </div>
+      </div>
 
-<div className='Iam'>
-  <h1>HI I'm <br/> Rubens Galarraga <br/> Web Developer </h1>
-  <p className='subtitulo'>Frontend Developer / Javascript / Vue js / React/ Node js </p>
- 
-    <NavLink to="/contact" className='contactme'> Contact me </NavLink>
-    <NavLink to="/contact" className='cv'> Download CV </NavLink>
-  
-  
-</div>
+      <div>
+        <img className="fotoperfil" src={foto} alt="" />
+      </div>
+    </div>
+  );
+};
 
-<div >
- <img className='fotoperfil' src={foto} alt=''/>
-
-</div>
-
-
-
-</div>
-
-
-
-    
-  )
-}
-
-export default Home
+export default Home;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,114 @@
-import React from 'react'
-import './Proyects.css'
+import React from "react";
+import "./Proyects.css";
 
 const Proyects = () => {
   return (
-    <div className='proyects'>
-<div> <h1> todolist</h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> Widget de clima </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>   <h1> Selector de emojis </h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>   <h1> Biblioteca </h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> Buscador  </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
+    <>
+      <div className="proyects">
+        <div className="grid">
+          <div className="grid__item">
+            <div className="card">
+              {" "}
+              <img
+                className="card__img"
+                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80"
+                alt="Snowy Mountains"
+              />
+              <div className="card__content">
+                <h1 className="card__header">E-comerce</h1>
+                <p className="card__text">
+                  Look up at the night sky, and find yourself{" "}
+                  <strong>immersed</strong> in the amazing mountain range of
+                  Aspen.
+                </p>
+                <button className="card__btn">
+                  Explore <span>&rarr;</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
-<div> <h1> Modal </h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> Acortador de URLs </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>   <h1> Calculadora </h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>   <h1> Tiktok  </h1>   <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> Componente estilo Notion  </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> App Pokemon  </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> App Movies  </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
-<div>  <h1> E-comerce </h1>    <p>  Hi there! My name is Agustin, and I am passionate and enthusiastic about creating on the web! My interest in development began when I decided to create a small website for a personal project with standard templates. Not being able to achieve the level of detail that I had I mind, I decided to get into the programming world, and since moment i did, I loved it! I like to write clean, tidy and work hard, I am currently looking to work in a place that allows me to grow, learn and progress in web development.</p></div>
 
 
-   
-    </div>
-  )
-}
 
-export default Proyects
+        <div className="grid">
+          <div className="grid__item">
+            <div className="card">
+              {" "}
+              <img
+                className="card__img"
+                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80"
+                alt="Snowy Mountains"
+              />
+              <div className="card__content">
+                <h1 className="card__header">App Pokemon</h1>
+                <p className="card__text">
+                  Look up at the night sky, and find yourself{" "}
+                  <strong>immersed</strong> in the amazing mountain range of
+                  Aspen.
+                </p>
+                <button className="card__btn">
+                  Explore <span>&rarr;</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="grid">
+          <div className="grid__item">
+            <div className="card">
+              {" "}
+              <img
+                className="card__img"
+                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80"
+                alt="Snowy Mountains"
+              />
+              <div className="card__content">
+                <h1 className="card__header">App Movies </h1>
+                <p className="card__text">
+                  Look up at the night sky, and find yourself{" "}
+                  <strong>immersed</strong> in the amazing mountain range of
+                  Aspen.
+                </p>
+                <button className="card__btn">
+                  Explore <span>&rarr;</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+       <div className="grid">
+          <div className="grid__item">
+            <div className="card">
+              {" "}
+              <img
+                className="card__img"
+                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80"
+                alt="Snowy Mountains"
+              />
+              <div className="card__content">
+                <h1 className="card__header">A starry night</h1>
+                <p className="card__text">
+                  Look up at the night sky, and find yourself{" "}
+                  <strong>immersed</strong> in the amazing mountain range of
+                  Aspen.
+                </p>
+                <button className="card__btn">
+                  Explore <span>&rarr;</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Proyects;
