388bd72d82b1a5485e392d1481cf7c0651e365fe
Rubens Galarraga <rgalarraga121@gmail.com>
2022-12-07 08:56:14 -0300

    add proyect

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import "./Proyects.css";
 import appMovies from  '../assets/appMovies.png'
 import pokemon from  '../assets/pokemon.png'
 import ecomerce from  '../assets/ecomerce.png'
+import pokedex from  '../assets/pokedex.png'
 
 
 const Proyects = () => {
@@ -88,29 +89,33 @@ const Proyects = () => {
 
 
 
-       <div className="grid">
+        <div className="grid">
           <div className="grid__item">
             <div className="card">
               {" "}
               <img
                 className="card__img"
-                src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80"
-                alt="Snowy Mountains"
+                src={pokedex}
+                alt="pokedex"
               />
               <div className="card__content">
-                <h1 className="card__header">A starry night</h1>
+                <h1 className="card__header">App Movies </h1>
                 <p className="card__text">
-                  Look up at the night sky, and find yourself{" "}
-                  <strong>immersed</strong> in the amazing mountain range of
-                  Aspen.
+                  Desarrollado con React + CreateReactApp,
+                  consumiendo una api de peliculas, realizado el deployment en Netlify.
                 </p>
-                <button className="card__btn">
-                  Explore <span>&rarr;</span>
-                </button>
+          <a href="https://pokedex-react2023.netlify.app/" target="blank_">   <button className="card__btn">
+              Explore   <span>&rarr;</span>
+                </button>  </a>
+                
+                   
               </div>
             </div>
           </div>
         </div>
+
+
+        
       </div>
     </>
   );
